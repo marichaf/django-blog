e661f6c278f9e7eb2dd996dd374d7fe1a8aa2997
marichaf <marichaf@gmail.com>
2022-06-05 19:36:52 -0700

    initial commit with polling list view update from class content

--- mysite/polling/urls.py
+++ mysite/polling/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from polling.views import list_view, detail_view
+from polling.views import PollListView, PollDetailView
 
 urlpatterns = [
-    path('', list_view, name="poll_index"),
-    path('polls/<int:poll_id>/', detail_view, name="poll_detail"),
+    path('', PollListView.as_view(), name="poll_index"), # note the difference in using a list view, need to add .as_view() - why parens?
+    path('polls/<int:pk>/', PollDetailView.as_view(), name="poll_detail"), 
+        # pk is primary key, generic detail view needs to use pk and not the specific name we had before
 ]
